024b4699a4edbcf1d7740b22542a942e5f85d285
arhammusheer <arhammusheer@gmail.com>
2021-10-20 23:47:23 -0400

    threejs

--- frontend/public/javascripts/accelerometer.js
+++ frontend/public/javascripts/accelerometer.js
@@ -1,25 +1,32 @@
 const gyroSupported = document.getElementById("gyro-supported");
-
-const accelerometer = new Accelerometer({ frequency: 60 });
+const socket = io();
+const accelerometer = new Accelerometer({ frequency: 10 });
 
 accelerometer.addEventListener("reading", (e) => {
   console.log(e);
   gyroSupported.innerHTML = `
 		<div class="m-1">
 			<p>
-				<strong>x:</strong> ${accelerometer.x}
+				<strong>x:</strong> ${Math.round(accelerometer.x)}
 			</p>
 			<p>
-				<strong>y:</strong> ${accelerometer.y}
+				<strong>y:</strong> ${Math.round(accelerometer.y)}
 			</p>
 			<p>
-				<strong>z:</strong> ${accelerometer.z}
+				<strong>z:</strong> ${Math.round(accelerometer.z)}
 			</p>
 			<p>
-				<strong>timestamp:</strong> ${accelerometer.timestamp}
+				<strong>timestamp:</strong> ${Math.round(accelerometer.timestamp)}
 			</p>
 		</div>
 	`;
+
+  socket.emit("accelerometer", {
+    x: Math.round(accelerometer.x),
+    y: Math.round(accelerometer.y),
+    z: Math.round(accelerometer.z),
+    timestamp: Math.round(accelerometer.timestamp),
+  });
 });
 
 accelerometer.start();
--- frontend/socketapi.js
+++ frontend/socketapi.js
@@ -6,6 +6,10 @@ const socketapi = {
 // Add your socket.io logic here!
 io.on("connection", function (socket) {
   console.log("A user connected");
+
+  socket.on("accelerometer", (data) => {
+    console.log(data);
+  });
 });
 // end of socket.io logic
 
