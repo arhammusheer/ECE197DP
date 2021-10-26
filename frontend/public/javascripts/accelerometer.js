2f651a71d679eb4f075a7ddcecaa085a68514c07
arhammusheer <arhammusheer@gmail.com>
2021-10-26 16:42:26 -0400

    huh

--- frontend/public/javascripts/accelerometer.js
+++ frontend/public/javascripts/accelerometer.js
@@ -1,9 +1,10 @@
 const gyroSupported = document.getElementById("gyro-supported");
 const info = document.getElementById("info");
 const count = document.getElementById("count");
+const arrow = document.getElementById("arrow");
 
 const socket = io();
-const accelerometer = new Accelerometer({ frequency: 10 });
+const accelerometer = new Accelerometer({ frequency: 100 });
 
 accelerometer.addEventListener("activate", (e) => {
   info.innerHTML = JSON.stringify(e);
@@ -43,6 +44,27 @@ accelerometer.addEventListener("reading", (e) => {
     y = Math.round(accelerometer.y);
     z = Math.round(accelerometer.z);
 
+    if (x == 0 && y == 0) {
+      showStop();
+    } else {
+      hideStop();
+
+      let temp_x = -1 * x;
+      let temp_y = -1 * y;
+      let direction = Math.atan(temp_y / temp_x);
+      if (temp_x < 0) {
+        direction += Math.PI;
+      } else if (temp_y < 0) {
+        direction += 2 * Math.PI;
+      }
+      info.innerHTML = `
+      direction: ${direction}
+      <br>
+      `;
+
+      arrow.style.transform = `rotate(${-direction}rad)`;
+    }
+
     socket.emit("accelerometer", {
       x: Math.round(accelerometer.x),
       y: Math.round(accelerometer.y),
@@ -53,13 +75,23 @@ accelerometer.addEventListener("reading", (e) => {
 });
 
 accelerometer.start();
-setTimeout(() => {
-  accelerometer.stop();
-  socket.emit("accelerometer", {
-    x: 0,
-    y: 0,
-    z: 10,
-    timestamp: Date.now(),
-  });
-  info.innerHTML = "Disabled\n";
-}, 10 * 1000);
+// setTimeout(() => {
+//   accelerometer.stop();
+//   socket.emit("accelerometer", {
+//     x: 0,
+//     y: 0,
+//     z: 10,
+//     timestamp: Date.now(),
+//   });
+//   info.innerHTML = "Disabled\n";
+// }, 10 * 1000);
+
+const hideStop = () => {
+  document.getElementById("stop-sign").style.display = "none";
+  document.getElementById("arrow").style.display = "block";
+};
+
+const showStop = () => {
+  document.getElementById("stop-sign").style.display = "block";
+  document.getElementById("arrow").style.display = "none";
+};
