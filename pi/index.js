ad52355791a17d2a2b3aa458c10ab40d7c602ca0
arhammusheer <arhammusheer@gmail.com>
2021-10-29 08:50:03 -0400

    yeh

--- pi/index.js
+++ pi/index.js
@@ -1,19 +1,37 @@
-let Gpio = require("pigpio");
+// let Gpio = require("pigpio");
 
-if (Gpio.initialize) {
-  Gpio = Gpio.Gpio;
-  console.log("Pigpio initialized");
-}
+// if (Gpio.initialize) {
+//   Gpio = Gpio.Gpio;
+//   console.log("Pigpio initialized");
+// }
 
-const led = new Gpio(17, { mode: Gpio.OUTPUT });
+// const led = new Gpio(17, { mode: Gpio.OUTPUT });
 
-let dutyCycle = 0;
+// let dutyCycle = 0;
 
-setInterval(() => {
-  led.pwmWrite(dutyCycle);
+// setInterval(() => {
+//   led.pwmWrite(dutyCycle);
 
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
+//   dutyCycle += 5;
+//   if (dutyCycle > 255) {
+//     dutyCycle = 0;
+//   }
+// }, 20);
+
+const io = require("socket.io-client");
+
+const socket = io("https://ece197dp.croissant.one", {
+  path: "/ece197dp",
+});
+
+socket.on("connect", () => {
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.on("disconnect", () => {
+  console.log(socket.id); // undefined
+});
+
+socket.on("accelerometer", (data) => {
+  console.log(data);
+});
---
+++ pi/simplified.js
@@ -0,0 +1,17 @@
+const io = require("socket.io-client");
+
+const socket = io("https://ece197dp.croissant.one", {
+  path: "/ece197dp",
+});
+
+socket.on("connect", () => {
+  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.on("disconnect", () => {
+  console.log(socket.id); // undefined
+});
+
+socket.on("remote", (data) => {
+  console.log(data);
+});
