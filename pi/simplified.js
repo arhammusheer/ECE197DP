166e128a6a5d0f0f7ee1410b9b2193975806012d
arhammusheer <arhammusheer@gmail.com>
2021-10-29 11:07:16 -0400

    whoops

--- pi/simplified.js
+++ pi/simplified.js
@@ -1,4 +1,13 @@
 const io = require("socket.io-client");
+require("dotenv").config();
+const Gpio = require("pigpio").Gpio;
+
+if (process.env.NODE_ENV === "production") {
+  const leftMotor1 = new Gpio(17, { mode: Gpio.OUTPUT });
+  const leftMotor2 = new Gpio(27, { mode: Gpio.OUTPUT });
+  const rightMotor1 = new Gpio(23, { mode: Gpio.OUTPUT });
+  const rightMotor2 = new Gpio(24, { mode: Gpio.OUTPUT });
+}
 
 const socket = io("https://ece197dp.croissant.one", {
   path: "/ece197dp",
@@ -14,5 +23,28 @@ socket.on("disconnect", () => {
 
 socket.on("simplified", (data) => {
   console.log(data);
-});
 
+  if (data.left > 0) {
+    if (process.env.NODE_ENV === "production") {
+      leftMotor1.pwmWrite((data.left / 100) * 255);
+      leftMotor2.pwmWrite(0);
+    }
+  } else {
+    if (process.env.NODE_ENV === "production") {
+      leftMotor1.pwmWrite(0);
+      leftMotor2.pwmWrite((-data.left / 100) * 255);
+    }
+  }
+
+  if (data.right > 0) {
+    if (process.env.NODE_ENV === "production") {
+      rightMotor1.pwmWrite((data.right / 100) * 255);
+      rightMotor2.pwmWrite(0);
+    }
+  } else {
+    if (process.env.NODE_ENV === "production") {
+      rightMotor1.pwmWrite(0);
+      rightMotor2.pwmWrite((-data.right / 100) * 255);
+    }
+  }
+});
